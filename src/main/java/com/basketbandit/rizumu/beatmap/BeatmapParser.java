03e6a8a400c08273e560a63dfda44206813b4034
BasketBandit <errorsfound@hotmail.co.jp>
2020-02-02 16:55:19 +0000

    re-wrote beatmap parser to check entire directory for yaml files instead of inputting single file

--- src/main/java/com/basketbandit/rizumu/beatmap/BeatmapParser.java
+++ src/main/java/com/basketbandit/rizumu/beatmap/BeatmapParser.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 
 import java.io.FileReader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 
 public class BeatmapParser {
@@ -11,9 +13,16 @@ public class BeatmapParser {
 
     public BeatmapParser(String path) {
         try {
-            beatmaps.addAll(new ObjectMapper(new YAMLFactory()).readValue(new FileReader(path), BeatmapContainer.class).getBeatmaps());
-        } catch(Exception e) {
-            e.printStackTrace();
+            // Checks all the files in the directory of the given path for files ending in .yaml, then tries to parse them as beatmaps.
+            Files.walk(Paths.get(path)).filter(file -> !file.getFileName().endsWith(".yaml")).forEach(s -> {
+                try {
+                    beatmaps.addAll(new ObjectMapper(new YAMLFactory()).readValue(new FileReader(s.toFile()), BeatmapContainer.class).getBeatmaps());
+                } catch(Exception ex) {
+                    ex.printStackTrace();
+                }
+            });
+        } catch(Exception ex) {
+            ex.printStackTrace();
         }
     }
 
