2ba92f55b50758fb882f430d5d37550e1aaecf4a
BasketBandit <errorsfound@hotmail.co.jp>
2020-02-03 02:54:30 +0000

    refactored render objects and moved ge stuff into the interface

--- src/main/java/com/basketbandit/rizumu/scene/RenderObject.java
+++ src/main/java/com/basketbandit/rizumu/scene/RenderObject.java
@@ -3,5 +3,8 @@ package com.basketbandit.rizumu.scene;
 import java.awt.*;
 
 public interface RenderObject {
+    GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+    Font[] fonts = ge.getAllFonts();
+
     void render(Graphics2D g);
 }
