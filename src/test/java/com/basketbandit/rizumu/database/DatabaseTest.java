2f517ee5319b1a4079618278007e0e27821cd6f8
BasketBandit <errorsfound@hotmail.co.jp>
2020-03-03 01:19:30 +0000

    implemented test cases for establishing connections, correct and incorrect login attempts

---
+++ src/test/java/com/basketbandit/rizumu/database/DatabaseTest.java
@@ -0,0 +1,29 @@
+package com.basketbandit.rizumu.database;
+
+import org.junit.Test;
+
+import java.sql.SQLException;
+
+/**
+ * Password should be hashed using BCrypt and a minimum of 12 rounds of generation for the salt.
+ * All Database functions are written assuming BCrypt is used for password hashing.
+ */
+public class DatabaseTest {
+    @Test
+    public void connectionEstablished() throws SQLException {
+        assert Connection.getConnection() != null;
+    }
+
+    @Test
+    public void loginCorrect() {
+        assert Database.login("josh", "test");
+    }
+
+    @Test
+    public void loginIncorrect() {
+        assert !Database.login("josh", " test");
+        assert !Database.login("josh", "test ");
+        assert !Database.login("josh", "Test");
+        assert !Database.login("josh", "wrong");
+    }
+}
